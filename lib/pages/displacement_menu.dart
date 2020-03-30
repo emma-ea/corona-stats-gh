76f6f04e93e89ad54fd186307792dc6236c082d8
oddlycoder <awuahemmanuel56@gmail.com>
2020-03-30 04:24:18 +0000

    displacement menu options tweak

--- lib/pages/displacement_menu.dart
+++ lib/pages/displacement_menu.dart
@@ -15,14 +15,12 @@ class _DisplacementMenuState extends State<DisplacementMenu> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Text('Home'),
-            Text('About COVID-19'),
-            Text('Statistics Currently'),
-            Text('Global Statistics'),
+            Text('Stay safe tips'),
             Text('Frequently Asked Questions'),
             Text('Press Releases'),
             Text('Other Releases'),
-            Text('About App')
+            Text('About App'),
+            Text('Credits'),
           ],
         ),
       ),
